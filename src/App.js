428afe768133ad8cd8e0e9d2c142f7c35413cac0
Tyler Maxwell <tylermaxwell661@gmail.com>
2021-08-10 07:01:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">this is my app on now</div>;
+  return <div className="App">this is my app on vercel</div>;
 }
 
 export default App;
